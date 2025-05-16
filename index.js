98c8edf5b1c43f4e9c4f4003759fdd219e363c12
Pablo Alan <pabloxt14@gmail.com>
2025-05-16 15:32:04 -0300

    feat: load project data on index page

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const overlay = document.getElementById("overlay");
 const menuOptions = mobileMenu.querySelectorAll(".menu-item");
 
 function toggleMenu() {
-	toggleButton.classList.toggle("active");
-	mobileMenu.classList.toggle("active");
-	overlay.classList.toggle("active");
+  toggleButton.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
 }
 
 function closeMenu() {
-	toggleButton.classList.remove("active");
-	mobileMenu.classList.remove("active");
-	overlay.classList.remove("active");
+  toggleButton.classList.remove("active");
+  mobileMenu.classList.remove("active");
+  overlay.classList.remove("active");
 }
 
 toggleButton.addEventListener("click", toggleMenu);
@@ -23,7 +23,7 @@ overlay.addEventListener("click", closeMenu);
 
 // biome-ignore lint/complexity/noForEach: <explanation>
 menuOptions.forEach((option) => {
-	option.addEventListener("click", closeMenu);
+  option.addEventListener("click", closeMenu);
 });
 
 /* ========== BACK TO TOP ========== */
@@ -31,10 +31,50 @@ menuOptions.forEach((option) => {
 const backToTopButton = document.getElementById("back-to-top-button");
 
 function handleScrollToTop() {
-	window.scrollTo({
-		top: 0,
-		behavior: "smooth",
-	});
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
 
 backToTopButton.addEventListener("click", handleScrollToTop);
+
+/* ========== LOAD PROJECTS ========== */
+
+import { PROJECTS } from "./data.js"
+
+const projectsContainer = document.getElementById("projects-grid");
+
+PROJECTS.forEach(project => {
+  const projectCard = `
+    <div class="project-card">
+      <img src="${project.image}" alt="${project.title}" class="project-image" />
+
+      <div class="project-date-stack">
+        <span>${project.date}</span>
+
+        <div class="tech-stack">
+          ${project.techStack.map((item) => item.icon).join('')}
+        </div>
+      </div>
+
+      <div class="project-description">
+        <strong>${project.title}</strong>
+
+        <p>${project.shortDescription}</p>
+      </div>
+
+      <div class="actions">
+        <a href="${project.repositoryUrl}" target="_blank" class="icon-button-primary">
+          <ion-icon name="logo-github"></ion-icon>
+        </a>
+
+        <a href="/project.html?id=${project.id}" class="icon-button-primary">
+          <i class="ph ph-arrow-up-right"></i>
+        </a>
+      </div>
+    </div>
+  `
+
+  projectsContainer.innerHTML += projectCard
+})
